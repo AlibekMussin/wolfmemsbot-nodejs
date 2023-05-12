afea087ee2d5e4bc16766b510bcf60fc6c5fefea
Алибек <svarog221989@gmail.com>
2023-05-12 18:02:08 +0600

    some front shit

--- public/index.js
+++ public/index.js
@@ -1,8 +1,9 @@
 const form = document.querySelector('#upload-form');
 
-
 const acceptButton = item.querySelector('.buttons button.accept-button');
 acceptButton.addEventListener('click', async () => {
+  const button = event.target;
+  const item = button.closest('.grid-item');
   console.log('accepted');
   const response = await fetch(`/memes/${meme._id}/accept`, { method: 'POST' });
   if (response.ok) {
--- routes/memeRoutes.js
+++ routes/memeRoutes.js
@@ -4,11 +4,13 @@ const MemeRequest = require('../models/MemeRequest');
 const fs = require('fs');
 
 
+
 router.get('/memes', async (req, res) => {
   try {
     const memes = await MemeRequest.find({
       //  status: 'APPROVED'
        });
+      //  console.log(memes);
     res.render('index', { memes });
   } catch (error) {
     console.log(error);
@@ -17,23 +19,28 @@ router.get('/memes', async (req, res) => {
 });
 
 router.post('/memes/:id/accept', async (req, res) => {
+  console.log('accept');
   try {
-    const meme = await MemeRequest.findByIdAndUpdate(req.params.id, { accepted: true });
-    res.json(meme);
+    // console.log(req.params);
+    const meme = await MemeRequest.findByIdAndUpdate(req.params.id, { status: 'APPROVED' });    
+    res.sendStatus(200);    
   } catch (error) {
     console.log(error);
     res.sendStatus(500);
   }
 });
 
-router.post('/memes/:id/decline', async (req, res) => {
-  try {
-    const meme = await MemeRequest.findByIdAndUpdate(req.params.id, { declined: true });
-    res.json(meme);
+router.post('/memes/:id/reject', async (req, res) => {
+  console.log('reject');
+  try {    
+    const meme = await MemeRequest.findByIdAndUpdate(req.params.id, { status: 'DECLINED' });    
+    res.sendStatus(200);
+    // console.log(meme);
   } catch (error) {
     console.log(error);
     res.sendStatus(500);
   }
 });
+
 
 module.exports = router;
